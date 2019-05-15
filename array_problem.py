91584be6b847c240f9dfe6eede703939dccd2b88
leeroee <13206411375@163.com>
2019-05-15 10:27:54 +0800

    123

---
+++ array_problem.py
@@ -0,0 +1,57 @@
+class Solution:
+    def maxArea(self, height: list) -> int:
+        '''
+        给定 n 个非负整数 a1，a2，...，an，每个数代表坐标中的一个点 (i, ai) 。
+        在坐标内画 n 条垂直线，垂直线 i 的两个端点分别为 (i, ai) 和 (i, 0)。
+        找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+        解法1：暴力搜索，但是会超出时间限制
+        maxarea = 0
+        for i in range(0, len(height)-1):
+            for j in range(i+1, len(height)):
+                maxarea = max(maxarea, (j-i)*min(height[i], height[j]))
+        return maxarea
+        '''
+        # 解法2，双指针：
+        maxarea, left, right = 0, 0, len(height)-1
+        while left < right:
+            maxarea = max(maxarea, (right-left)*min(height[left], height[right]))
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxarea
+    
+    def threeSum(self, nums: list) -> list:
+        '''
+        求三数之和，取数组中取三个数，和为0，返回所有这样的三元组，不能重复
+        '''
+        res = []
+        cnt = len(nums)
+        if cnt < 3: return res      # 数组个数小于3直接返回空
+        nums.sort()                 # 先排序
+        for left in range(0, cnt-2):                        # 从数组中最小的数遍历至导数第三个
+            if left > 0 and nums[left] == nums[left-1]:     # 如果left和前一个相等跳过，因为完全一样
+                continue
+            if nums[left] > 0: break                        # left>0返回，因为三个大于零的数和必然大于0
+            mid = left + 1
+            right = cnt - 1
+            while mid < right:
+                s = nums[left] + nums[mid] + nums[right]
+                if s == 0:
+                    res.append((nums[left], nums[mid], nums[right]))
+                    while mid < right and nums[mid] == nums[mid+1]:
+                        mid += 1                            # 跳过相同的数字，避免重复
+                    while right > mid and nums[right] == nums[right-1]: 
+                        right -= 1                              
+                    mid += 1
+                    right -= 1
+                elif s < 0:
+                    mid += 1
+                else:
+                    right -= 1
+                    if nums[right] < 0: break               # right<0跳出，三个小于零的数和必然小于零
+        return res
+
+if __name__ == "__main__":
+    so = Solution()
+    print(so.threeSum([-1,0,1,2,-1,-4]))
--- tx50.py
+++ tx50.py
@@ -1,5 +1,5 @@
 class Solution(object):
-    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+    def findMedianSortedArrays(self, nums1, nums2) -> float:
         '''
         给定两个大小为 m 和 n 的有序数组 nums1 和 nums2。
         请你找出这两个有序数组的中位数，并且要求算法的时间复杂度为 O(log(m + n))。
@@ -35,8 +35,27 @@ class Solution(object):
                 
                 return (max_left + max_right) / 2
 
+    def longestPalindrome(self, s: str) -> str:
+        '''
+        给定一个字符串 s，找到 s 中最长的回文子串。你可以假设 s 的最大长度为 1000。
+        '''
+        if len(s) < 2: return s
+        start = end = 0
+        for i in range(len(s)):
+            len1 = self.get_expand_length(i, i, s)
+            len2 = self.get_expand_length(i, i+1, s)
+            maxlen = max(len1, len2)
+            if maxlen > end - start:
+                start = i - (maxlen-1) // 2
+                end = i + maxlen // 2 + 1
+        return s[start:end]
+
+    def get_expand_length(self, left, right, s):
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 1
 
 if __name__ == "__main__":
     so = Solution()
-    med = so.median([1,3], [2])
-    print(med)
+    print(so.longestPalindrome('sfwegabcbahsajkwiv'))
